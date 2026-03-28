31a865f15ba68e9f88bb9351a815fdd006275aa3
kenjicomputer90-rgb <kenjicomputer90@gmail.com>
2026-03-28 18:59:27 -0300

    reacrescentando funções do produtos.ts 2

--- src/modulos/produtos.ts
+++ src/modulos/produtos.ts
@@ -1,3 +1,10 @@
+export function addProduto(nome:string, material:string, descricao:string, preco:number, status:"alugado"|"disponivel"|"a venda", tamanho:number , cor:string){}
 export function addProduto(nome:string, material:string, descricao:string, preco:number, status:string){}
 export function removeProduto(id:number){}
+export function changeProduto(id:number, nome:string, material:string, descricao:string, preco:number, status:string){}
+export function getProduto(id:number){}
+export function precificaProduto(){}
+export function porcentagem_venda(tipo: ){
+
+}
 export function changeProduto(id:number, nome?:string, material?:string, descricao?:string, preco?:number, status?:string){}
