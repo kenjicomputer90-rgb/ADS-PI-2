0efb207d2290aba87caf74748dc108e085daf9f6
kenjicomputer90-rgb <kenjicomputer90@gmail.com>
2026-03-28 17:13:15 -0300

    adicionando funções do modulo cliente

---
+++ src/modulos/cliente.ts
@@ -0,0 +1,9 @@
+export function AddCliente(nome:string, cpf:number, rg:number, data_nacimento:string){
+
+}
+export function RemoveCliente(id:number){
+
+}
+export function ChangeCliente(id:number, nome?:string, cpf?:number, rg?:number, data_nacimento?:string){
+
+}
