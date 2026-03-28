954c4bb4174549600ef805f76cf686e96006df39
Takeshi-hub7 <guilherme.takeshi.km@gmail.com>
2026-03-28 17:15:36 -0300

    definir campos de  dados do produto

---
+++ src/modulos/produtos.ts
@@ -0,0 +1,3 @@
+export function addProduto(nome:string, material:string, descricao:string, preco:number, status:string){}
+export function removeProduto(id:number){}
+export function changeProduto(id:number, nome:string, material:string, descricao:string, preco:number, status:string){}
