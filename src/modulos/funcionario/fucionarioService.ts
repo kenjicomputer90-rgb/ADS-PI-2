e8d632b9a0c78cd9607bcdcf2fdb95fcbe0a0535
Takeshi-hub7 <guilherme.takeshi.km@gmail.com>
2026-05-23 15:53:09 -0300

    importa prisma para funcionarios

--- src/modulos/funcionario/fucionarioService.ts
+++ src/modulos/funcionario/fucionarioService.ts
@@ -1,3 +1,14 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./banco/dev.db"
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
 export function addFuncionario(nome:string, cpf:number, cargo:string, rg: number, email:string, telefone:number, cpts:number,dependentes:number,sexo: string,salario:number, data_de_nascimento:string, estado_civil:string){}
 export function removeFuncionario(id:number){}
 export function changeFuncionario(id:number,nome?:string, cpf?:number, rg?: number, email?:string,telefone?:number, cpts?:number,dependentes?:number,sexo?: string,salario?:number, data_de_nascimento?:string, estado_civil?:string){}
