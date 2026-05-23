4fefa885e85b93095fed224089e7608e950f4710
rochats <rochats6@outlook.com>
2026-05-23 16:48:13 -0300

    Adicionei a funcao 'adicionar cliente' no arquivo clienteService

--- src/modulos/cliente/clienteService.ts
+++ src/modulos/cliente/clienteService.ts
@@ -1,6 +1,27 @@
-export function addCliente(nome:string, cpf:number, rg:number, data_nascimento:string){
-    const novoCliente= nome+cpf+rg+data_nascimento
-    return novoCliente
+import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./banco/dev.db"
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export async function addCliente(nome:string, cpf:string,telefone: string,
+     endereco:string, rg?:string, data_nascimento?:string)
+{
+  const newCliente = await prisma.cliente.create({
+    data: {
+        nome: nome,
+        cpf: cpf,
+        rg: rg,
+        telefone: telefone,
+        data_nascimento: data_nascimento,
+        endereco: endereco,
+    }
+  })
 }
 export function removeCliente(id:number){
 
